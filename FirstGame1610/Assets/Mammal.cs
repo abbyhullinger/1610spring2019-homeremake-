456e50c1c1d29c9532b4d34d7180bc7732fb660b
Abigail Hullinger <10804479@my.uvu.edu>
2019-02-11 22:56:59 -0700

    Health, Speed, CanMove, FurCount, EatingSpeed, CanEat

---
+++ FirstGame1610/Assets/Mammal.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mammal : Animal {
+public int FurCount = 10000; 
+public float EatingSpeed = 0.4f;
+public bool CanEat = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
