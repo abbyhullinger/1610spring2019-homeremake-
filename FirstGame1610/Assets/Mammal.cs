d63e9122156824b7e4b093decbaa721efa154c99
Abigail Hullinger <10804479@my.uvu.edu>
2019-02-12 19:13:48 -0700

    UnityEvent and Unity Event Invoke for Mammal
    On teddybear

--- FirstGame1610/Assets/Mammal.cs
+++ FirstGame1610/Assets/Mammal.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Mammal : Animal {
 public int FurCount = 10000; 
 public float EatingSpeed = 0.4f;
 public bool CanEat = true;
+public UnityEvent Event; 
 
 	// Use this for initialization
 	void Start () {
-		
+ Event.Invoke();
 	}
 	
 	// Update is called once per frame
