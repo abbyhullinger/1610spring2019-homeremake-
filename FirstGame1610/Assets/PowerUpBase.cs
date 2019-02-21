b9ee894bfd7b7e5765fa652ca54addef0a0b4fd9
Abigail Hullinger <10804479@my.uvu.edu>
2019-02-20 22:01:16 -0700

    PowerUpBase, AmmoLevel, MagicLevel, Added folders for things
    added apple and bread

---
+++ FirstGame1610/Assets/PowerUpBase.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class PowerUpBase : ScriptableObject{
+public float Value = 10;
+
+	void OnEnable () {
+	
+	}
+	
+}
