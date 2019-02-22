d5003c24f4f36ee223217a500c6b7ac1ae76cfa5
Abigail Hullinger <10804479@my.uvu.edu>
2019-02-21 23:49:41 -0700

    Added clouds, tried to script cloud color
    cloud color didn't work, try again

---
+++ FirstGame1610/Assets/Scripts/Clouds.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clouds : BackgroundObjects {
+public Color CloudColor;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
