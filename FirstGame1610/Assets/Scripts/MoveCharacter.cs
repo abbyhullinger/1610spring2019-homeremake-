9a3b6d70ff87752604e11ba47a285ec7f25efb34
Abigail Hullinger <10804479@my.uvu.edu>
2019-03-07 23:36:29 -0700

    Start of Gravity Script

--- FirstGame1610/Assets/Scripts/MoveCharacter.cs
+++ FirstGame1610/Assets/Scripts/MoveCharacter.cs
@@ -7,6 +7,7 @@ public class MoveCharacter : MonoBehaviour
 {
 	public UnityEvent OnGrounded, OffGrounded;
 	public float Speed = 3;
+	public float Gravity = -1;
 	
 	private CharacterController _controller;
 	private Vector3 position; 
@@ -29,8 +30,17 @@ public class MoveCharacter : MonoBehaviour
 		{
 			OffGrounded.Invoke();
 		}
-		position.y = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
+
+		if (Input.GetAxis("Vertical) > 0 || Input.GetAxis("Vertical"")
+		{
+			position.y = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
+		}
+		
+
+		position.y = Gravity * Time.deltaTime;
+		
 		position.x = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+		
 		_controller.Move(position);
 	}
 }
