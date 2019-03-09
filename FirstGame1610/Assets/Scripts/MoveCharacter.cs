a294064ede49f654c0340ae8a9b3b774960d8ea1
Abigail Hullinger <10804479@my.uvu.edu>
2019-03-08 23:46:27 -0700

    Updated MoveCharacter script with Jump

--- FirstGame1610/Assets/Scripts/MoveCharacter.cs
+++ FirstGame1610/Assets/Scripts/MoveCharacter.cs
@@ -6,8 +6,10 @@ using UnityEngine.Events;
 public class MoveCharacter : MonoBehaviour
 {
 	public UnityEvent OnGrounded, OffGrounded;
-	public float Speed = 3;
-
+	public float Speed = 30;
+	public float Gravity = -3;
+	public float JumpForce = 2;
+	
 	private CharacterController controller;
 	private Vector3 position;
 	
@@ -28,8 +30,28 @@ public class MoveCharacter : MonoBehaviour
 			OffGrounded.Invoke();
 		}
 
-		position.y = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
+		if (controller.isGrounded && Input.GetButton("Jump"))
+		{
+			position.y = JumpForce; 
+		}
+
+		position.y += Gravity * Time.deltaTime;
+		
+
+//		if (Input.GetAxis("Vertical") > 0 || Input.GetAxis("Vertical") < 0)
+//		{
+//			position.y = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
+//		}
+//		else
+//		{
+//			position.y = Gravity * Time.deltaTime;
+//		}
+		
+		 
+		
 		position.x = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+
+		
 
 		controller.Move(position);
 	}
