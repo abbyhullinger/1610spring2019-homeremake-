54f5ee900b5af8766c2d197e97b309149a936ddf
Abigail Hullinger <10804479@my.uvu.edu>
2019-03-08 23:05:35 -0700

    New movecharacter script.. something went wrong with the first one

--- FirstGame1610/Assets/Scripts/MoveCharacter.cs
+++ FirstGame1610/Assets/Scripts/MoveCharacter.cs
@@ -7,22 +7,19 @@ public class MoveCharacter : MonoBehaviour
 {
 	public UnityEvent OnGrounded, OffGrounded;
 	public float Speed = 3;
-	public float Gravity = -1;
+
+	private CharacterController controller;
+	private Vector3 position;
 	
-	private CharacterController _controller;
-	private Vector3 position; 
-
-
-
 	void Start ()
 	{
-		_controller = GetComponent<CharacterController>();
+		controller = GetComponent<CharacterController>();
 	}
 	
 
-	void Update ()
+	void Update () 
 	{
-		if (_controller.isGrounded)
+		if (controller.isGrounded)
 		{
 			OnGrounded.Invoke();
 		}
@@ -31,16 +28,9 @@ public class MoveCharacter : MonoBehaviour
 			OffGrounded.Invoke();
 		}
 
-		if (Input.GetAxis("Vertical) > 0 || Input.GetAxis("Vertical"")
-		{
-			position.y = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
-		}
-		
-
-		position.y = Gravity * Time.deltaTime;
-		
+		position.y = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
 		position.x = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
-		
-		_controller.Move(position);
+
+		controller.Move(position);
 	}
 }
