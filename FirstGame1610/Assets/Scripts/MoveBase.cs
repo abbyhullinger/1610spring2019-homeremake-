3727d610befba32a8c4998e90015acfcd6aa2243
Abigail Hullinger <10804479@my.uvu.edu>
2019-03-09 00:19:06 -0700

    Added scripts MoveBase, MoveSingleJump, MoveWithAxis, edited MoveCharacter

---
+++ FirstGame1610/Assets/Scripts/MoveBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MoveBase : ScriptableObject
+{
+	protected Vector3 position;
+	public float Speed = 30;
+	public float JumpForce = 1.5f;
+	public float Gravity = -3;
+
+	public abstract void Move(CharacterController controller);
+
+
+}
--- FirstGame1610/Assets/Scripts/MoveCharacter.cs
+++ FirstGame1610/Assets/Scripts/MoveCharacter.cs
@@ -6,12 +6,9 @@ using UnityEngine.Events;
 public class MoveCharacter : MonoBehaviour
 {
 	public UnityEvent OnGrounded, OffGrounded;
-	public float Speed = 30;
-	public float Gravity = -3;
-	public float JumpForce = 2;
-	
+	public MoveBase CharacterMover;
 	private CharacterController controller;
-	private Vector3 position;
+	
 	
 	void Start ()
 	{
@@ -30,29 +27,9 @@ public class MoveCharacter : MonoBehaviour
 			OffGrounded.Invoke();
 		}
 
-		if (controller.isGrounded && Input.GetButton("Jump"))
-		{
-			position.y = JumpForce; 
-		}
+		CharacterMover.Move(controller);
 
-		position.y += Gravity * Time.deltaTime;
-		
 
-//		if (Input.GetAxis("Vertical") > 0 || Input.GetAxis("Vertical") < 0)
-//		{
-//			position.y = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
-//		}
-//		else
-//		{
-//			position.y = Gravity * Time.deltaTime;
-//		}
-		
-		 
-		
-		position.x = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
 
-		
-
-		controller.Move(position);
 	}
 }
