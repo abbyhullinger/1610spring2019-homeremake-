3cf385e83d5a06dd4f45a4cc0f7d5cbea44b58e8
Abigail Hullinger <10804479@my.uvu.edu>
2019-03-09 23:48:06 -0600

    Updated monoevents script added ammo

--- FirstGame1610/Assets/Scripts/MonoEvents.cs
+++ FirstGame1610/Assets/Scripts/MonoEvents.cs
@@ -4,22 +4,16 @@ using UnityEngine;
 using UnityEngine.Events;
 public class MonoEvents : MonoBehaviour {
 
-	public UnityEvent StartEvent, EnableEvent, MouseUpEvent, UpdateEvent;
-	void Start () {
+	public UnityEvent StartEvent, EnableEvent, TriggerEnterEvent, UpdateEvent;
+	private void Start () 
+	{
 	StartEvent.Invoke();
 	}
-	private void OnEnable() {
-	print("Enabled"); 
-	EnableEvent.Invoke();
+	private void OnTriggerEnter(Collider other) 
+	{ 
+	TriggerEnterEvent.Invoke();
 	}
 
-	private void OnMouseUp() {
-		print("MouseUp");
-		MouseUpEvent.Invoke();
-		gameObject.active = false; 
-	}
-	
-	// Update is called once per frame
 	void Update () {
 	UpdateEvent.Invoke();
 		
