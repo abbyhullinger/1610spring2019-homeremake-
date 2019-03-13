bcc343e8d25bdf77a99243edcf3233a351a615d1
Abigail Hullinger <10804479@my.uvu.edu>
2019-03-13 16:04:50 -0600

    Ammo prefab, float data start to edit, updated Unity, updated editor

--- FirstGame1610/Assets/Scripts/FloatData.cs
+++ FirstGame1610/Assets/Scripts/FloatData.cs
@@ -3,7 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu]
-public class FloatData : ScriptableObject {
+public class FloatData : ScriptableObject
+{
+    [SerializeField] private float value;
 
-public float Value; 
+    public float Value
+    {
+        get { return value; }
+        set
+        {
+            value
+        }
+    }
 }
+
