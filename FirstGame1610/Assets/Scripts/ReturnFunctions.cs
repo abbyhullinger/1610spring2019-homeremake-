006a9a93d4e4eb621e98b070f65a7dcd888796dd
Abigail Hullinger <10804479@my.uvu.edu>
2019-03-16 23:19:12 -0600

    A, B, C (Add and SUbtract) In return function

--- FirstGame1610/Assets/Scripts/ReturnFunctions.cs
+++ FirstGame1610/Assets/Scripts/ReturnFunctions.cs
@@ -4,11 +4,34 @@ using UnityEngine;
 
 public class ReturnFunctions : MonoBehaviour
 {
+    public int A;
+    public int B;
+    public int C;
+
+    public Vector3 position;
+    public CharacterController Controller;
+
+    private int Add()
+    {
+        return A + B;
+    }
     
+    private int Subtract()
+    {
+        return A - B;
+    }
+
+    public Vector3 Move()
+    {
+        position.y = Input.GetAxis("Horizontal");
+        Controller.Move(position);
+        return position;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        C = Add();
+        C = Subtract();
     }
 
     // Update is called once per frame
