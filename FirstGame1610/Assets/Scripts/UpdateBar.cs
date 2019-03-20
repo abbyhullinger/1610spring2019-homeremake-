361e93bd296cfcc6e2fae35510bcecf41228be14
Abigail Hullinger <10804479@my.uvu.edu>
2019-03-19 23:48:56 -0600

    Added GameUI, UpdateBar script. For some reason, the other sprites are
    In this one, I don't know why

---
+++ FirstGame1610/Assets/Scripts/UpdateBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using Image = UnityEngine.UI.Image;
+
+public class UpdateBar : MonoBehaviour
+{
+    private Image BarImage;
+
+    public FloatData FillNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BarImage = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BarImage.fillAmount = FillNumber.Value;
+    }
+}
