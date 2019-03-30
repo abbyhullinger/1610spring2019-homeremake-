a53a5a8ee59d5fe6640dd43734693779629f340a
Abigail Hullinger <10804479@my.uvu.edu>
2019-03-29 23:18:39 -0600

    Added start to enemy script, added damage

---
+++ FirstGame1610/Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MoveCharacter
+{
+    public int HealthDamage;
+
+    private Animator animator;
+
+    private Transform target;
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        animator = GetComponent<Animator>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
