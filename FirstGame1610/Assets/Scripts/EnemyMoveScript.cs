4e1704323566668ff88b5fc672c43777e363146f
Abigail Hullinger <10804479@my.uvu.edu>
2019-03-30 23:04:23 -0600

    Enemymovescript I found online i wanted to try

---
+++ FirstGame1610/Assets/Scripts/EnemyMoveScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMoveScript : MonoBehaviour
+{
+    public Vector2 speed = new Vector2(10, 10);
+    public Vector2 direction = new Vector2(-1, 0);
+
+    private Vector2 movement;
+
+    private Rigidbody rigidbodyComponent;
+    
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector2(
+            speed.x * direction.x,
+            speed.y * direction.y  );
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (rigidbodyComponent == null) rigidbodyComponent = GetComponent<Rigidbody>();
+        rigidbodyComponent.velocity = movement;
+    }
+}
