afec8d6310eb9d2e2af565cf3f453c0dc705ff60
Abigail Hullinger <10804479@my.uvu.edu>
2019-04-04 23:47:46 -0600

    trying to make door script:) just the very start

---
+++ FirstGame1610/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OpenDoor : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool Door
+    public GameObject Key
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+   public void Update()
+    {
+        if (Door())
+        {
+          
+        }
+    }
+}
