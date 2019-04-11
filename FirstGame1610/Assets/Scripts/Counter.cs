21cb15041df9d44de0c3ed4506303b2922baec48
Abigail Hullinger <10804479@my.uvu.edu>
2019-04-10 22:03:22 -0600

    Start to forces script

---
+++ FirstGame1610/Assets/Scripts/Counter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Counter : MonoBehaviour
+{
+    public int Value = 3;
+    // Start is called before the first frame update
+    IEnumerable<WaitForSeconds> Start()
+    {
+        while (Value > 0)
+        {
+            yield return new WaitForSeconds(2);
+            print(Value);
+            Value--;
+        }
+    }
+
+
+}
